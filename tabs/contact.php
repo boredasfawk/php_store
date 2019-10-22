d99b06f33c29a98a3f4e2f261ee6f8cfeb69c2f5
boredasfawk <olonnyetaylor@gmail.com>
2019-10-21 20:30:38 -0700

    finished contact and made functional auto-reply email

--- tabs/contact.php
+++ tabs/contact.php
@@ -20,7 +20,6 @@
 <!-- Contact Begin -->
 <div id="content">
   <div class="container">
-
     <div class="col-md-12">
 
       <!-- Crumb Begin -->
@@ -48,9 +47,7 @@
 
     <!-- Contact-Content Begin -->
     <div class="col-md-9">
-
       <div class="box">
-
         <div class="box-header">
 
           <center>
@@ -62,11 +59,9 @@
               If you have any questions, feel free to contact us. Our Customer Service work <strong>24/7</strong>
 
             </p>
-
           </center>
 
           <form action="contact.php" method="post">
-
             <div class="form-group">
 
               <label>Name</label>
@@ -106,15 +101,47 @@
                 <i class="fa fa-user-md"></i>Send Message
 
               </button>
-
             </div>
-
           </form>
-
+          <?php 
+            // checks if form has been submitted           
+            if(isset($_POST['submit'])){
+              // admin portion  
+              
+              // grabs info from form and puts into variables
+              // uses admins email to recieve emails
+              $sender_name = $_POST['name'];
+              
+              $sender_email = $_POST['email'];
+              
+              $sender_subject = $_POST['subject'];
+              
+              $sender_message = $_POST['message'];
+              
+              $receiver_email = "olonnyetaylor@gmail.com";
+              
+              mail($receiver_email,$sender_name,$sender_subject,$sender_message,$sender_email);
+              
+              // auto reply to sender 
+              //grab email from form
+              // put msg to sender in variables to set in mail function
+              // return html to confirm email 
+              $email = $_POST['email'];
+              
+              $subject = "Welcome to my website";
+              
+              $msg = "Thank you for reaching out. I will personally get back to you within 24 - 48hrs";
+              
+              $from = "olonnyetaylor@gmail.com";
+              
+              mail($email,$subject,$msg,$from);
+              
+              echo "<h2 align='center'> Your message has sent sucessfully </h2>";
+                
+            }           
+          ?>
         </div>
-
       </div>
-
     </div>
     <!-- Contact-Content Finish -->
   </div>
@@ -122,6 +149,6 @@
 <!-- Contact Finish -->
 <?php
 
-    include __DIR__ . "/../includes/footer.php";
+  include __DIR__ . "/../includes/footer.php";
     
-  ?>
+?>
